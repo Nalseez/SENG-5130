f17b0974e20c690ec4aa3f4726c5b4758ffd2b1b
Nalseez <dukex048@umn.edu>
2020-04-30 23:48:38 -0500

    some changes

--- index.js
+++ index.js
@@ -10,35 +10,40 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.get('/turn-on', function(req, res){
+  loginAndPublishEvent('turn-on');
+  res.send('all done');
+})
+
 //Launch listening server on port 8081
 app.listen(8081, function () {
   console.log('app listening on port 8081!')
 })
 
-
-particle.login({username: 'USERNAME', password: 'PASSWORD'}).then(
-  function(data){
-    token = data.body.access_token;
-    return token;
-  },
-  function(err){
-    console.log('Could not log in.', err);
-  }
-).then(
-  function(result){
-    return particle.publishEvent({ name: 'toggle-water-pump', data: 'turn-off', auth: token});
-  },
-  function(err){
-    console.log('Error when publishing event.', err);
-  }
-).then(
-  function(data){
-    if(data.body.ok){
-      console.log("Event published successfully");
-    }
-  },
+function loginAndPublishEvent(eventName){
+  particle.login({username: 'USERNAME', password: 'PASSWORD'}).then(
+    function(data){
+      token = data.body.access_token;
+      return token;
+    },
     function(err){
-      console.log("Failed to publish event: " + err);
+      console.log('Could not log in.', err);
     }
-);
-
+  ).then(
+    function(result){
+      return particle.publishEvent({ name: 'toggle-water-pump', data: 'turn-off', auth: token});
+    },
+    function(err){
+      console.log('Error when publishing event.', err);
+    }
+  ).then(
+    function(data){
+      if(data.body.ok){
+        console.log("Event published successfully");
+      }
+    },
+      function(err){
+        console.log("Failed to publish event: " + err);
+      }
+  );
+}
